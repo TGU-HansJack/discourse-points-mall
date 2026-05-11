f62e6acdf52e9fc600c594a013863d57cbf049ea
HansJack <2310210201@tiangong.edu.cn>
2026-05-11 21:32:19 +0800

    优化积分管理逻辑，支持新的排行榜积分计算方式，并调整样式以改善用户界面

--- lib/discourse_points_mall/points_manager.rb
+++ lib/discourse_points_mall/points_manager.rb
@@ -4,14 +4,26 @@ module ::DiscoursePointsMall
   class PointsManager
     def self.enabled?
       defined?(::DiscourseGamification::GamificationScoreEvent) &&
-        defined?(::DiscourseGamification::GamificationScore)
+        defined?(::DiscourseGamification::GamificationLeaderboardScore) &&
+        defined?(::DiscourseGamification::LeaderboardCachedView) &&
+        defined?(::DiscourseGamification::GamificationLeaderboard)
     end
 
     def self.balance_for(user)
       return 0 if user.blank?
-      return 0 unless defined?(::DiscourseGamification::GamificationScore)
+      return 0 unless enabled?
 
-      ::DiscourseGamification::GamificationScore.where(user_id: user.id).sum(:score).to_i
+      if user.respond_to?(:gamification_score)
+        user.gamification_score.to_i
+      else
+        default_leaderboard = ::DiscourseGamification::GamificationLeaderboard.select(:id).first
+        return 0 unless default_leaderboard
+
+        ::DiscourseGamification::GamificationLeaderboardScore.where(
+          leaderboard_id: default_leaderboard.id,
+          user_id: user.id,
+        ).sum(:score).to_i
+      end
     rescue StandardError
       0
     end
@@ -29,7 +41,8 @@ module ::DiscoursePointsMall
         points: points,
         description: description,
       )
-      ::DiscourseGamification::GamificationScore.calculate_scores(since_date: today)
+      ::DiscourseGamification::GamificationLeaderboardScore.calculate_all(since_date: today)
+      ::DiscourseGamification::LeaderboardCachedView.regenerate_all
       true
     rescue => e
       Rails.logger.warn("DiscoursePointsMall: 积分写入失败 - #{e.class}: #{e.message}")
